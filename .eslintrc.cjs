8b2b1209664d93b61a1084c879177c1cf0d645cd
Skeith <github@skeith.dev>
2023-08-02 19:18:48 +0800

    Add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,11 @@
 module.exports = {
+  plugins: ['prettier'],
   extends: [
     'next/core-web-vitals',
     'plugin:@next/next/recommended',
     'airbnb',
     'airbnb-typescript',
+    'prettier',
   ],
   rules: {
     'react/react-in-jsx-scope': 'off',
